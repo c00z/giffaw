1d59eca27a31f0f2d6a5654f811d66ec8682b8e0
c00z <zcusimano@gmail.com>
2016-12-06 18:02:56 -0800

    Top 25 dispaly on page

--- scripts/app.js
+++ scripts/app.js
@@ -10,32 +10,40 @@ $(document).on("ready", function(){
       complete: onCompletion
     })
 
-
 //form submit search
-  // $('form').on('submit'), searchReq);
-  // function searchReq(event) {
-  // 	event.preventDefault(); // might not need w/ button
-  // 	$.ajax( {
-  // 		method:"GET",
-  // 		url: "http://api.giphy.com/v1/gifs/search".
-  // 		dataType: "json",
-  // 		data: "q"
-  //     success: onSuccess
-  //     error: onError
-  //     complete: completedCallBack,
-  //
-  // 		})
-  // }
+$("form").on("submit", searchReq)
+  function searchReq(event) {
+  	event.preventDefault(); // might not need w/ button
+  	$.ajax( {
+  	  method:"GET",
+  		url: "http://api.giphy.com/v1/gifs/search",
+  	  dataType: "json",
+  		data: $("form").serialize(),
+      success: onSuccess,
+      error: onError,
+      complete: onCompletion,
+
+  		})
+  }
 
 //acccess to response, success call back example from notes.
 //foreach
   function onSuccess(responseData){
     console.log(responseData);
-    $('.gif-gallery').append(responseData);
+    var imageArray = responseData.data;
+    imageArray.forEach(function(object) {
+    $('.gif-gallery').append('<img src="' + object.images.fixed_height.url + '">')
+  });
+
+
 
   };
 
   function onError(){
+  alert("Sorry, there was a problem!");
+  console.log("Error: " + errorThrown);
+  console.log("Status: " + status);
+  console.dir(xhr);
 
   };
 
